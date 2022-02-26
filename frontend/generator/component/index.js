7b1e199393e05cae7a21c42cbbf99a4f4418dcf1
D_nishimura <11kakeru30@gmail.com>
2022-02-26 23:33:22 +0900

    update: generator

--- frontend/generator/component/index.js
+++ frontend/generator/component/index.js
@@ -1,85 +1,62 @@
-const path = require('path')
-const fs = require('fs')
-
-const existElementsIndexFile = () => {
-  return fs.existsSync(
-    path.resolve(__dirname, '../../src/components/Elements/index.ts')
-  )
-}
-
-module.exports = {
-  description: 'src/components/*',
-  prompts: [
-    {
-      type: 'list',
-      name: 'choice',
-      message: 'どこにコンポーネントを作成しますか？',
-      choices: ['components/*', 'components/Elements/*']
-    },
-    {
-      type: 'input',
-      name: 'compoent',
-      message: 'pascalCase component name'
-    }
-  ],
-  actions: ({ choice }) => {
-    const actions = []
-    if (choice === 'components/*') {
-      actions.push(
-        {
-          type: 'add',
-          path: 'src/components/{{pascalCase compoent}}/{{pascalCase compoent}}.tsx',
-          templateFile: 'generator/component/Component.tsx.hbs'
-        },
-        {
-          type: 'add',
-          path: 'src/components/{{pascalCase compoent}}/index.ts',
-          templateFile: 'generator/component/index.ts.hbs'
-        }
-      )
-
-      return actions
-    }
-
-    if (choice === 'components/Elements/*') {
-      actions.push(
-        {
-          type: 'add',
-          path: 'src/components/Elements/{{pascalCase compoent}}/index.ts',
-          templateFile: 'generator/component/index.ts.hbs'
-        },
-        {
-          type: 'add',
-          path: 'src/components/Elements/{{pascalCase compoent}}/{{pascalCase compoent}}.tsx',
-          templateFile: 'generator/component/Component.tsx.hbs'
-        }
-      )
-
-      existElementsIndexFile()
-        ? actions.push({
-            type: 'modify',
-            path: 'src/components/Elements/index.ts',
-            templateFile: 'generator/component/index.ts.hbs',
-            transform: (current, { compoent }) => {
-              const currentModules = current.trim()
-              const createReExportModulePath = moduleName =>
-                `export * from './${moduleName}'`
-              const addedModules =
-                currentModules +
-                '\n' +
-                createReExportModulePath(compoent) +
-                '\n'
-
-              return Promise.resolve(addedModules)
-            }
-          })
-        : actions.push({
+module.exports = function (
+  name = 'components',
+  /** @type {import('plop').NodePlopAPI} */
+  plop
+) {
+  plop.setGenerator(name, {
+    description: 'src/components/*',
+    prompts: [
+      {
+        type: 'list',
+        name: 'choice',
+        message: 'どこにコンポーネントを作成しますか？',
+        choices: ['components/*', 'components/Elements/*']
+      },
+      {
+        type: 'input',
+        name: 'component',
+        message: 'pascalCase component name'
+      }
+    ],
+    actions: ({ choice, component }) => {
+      const actions = []
+      if (choice === 'components/*') {
+        actions.push(
+          {
             type: 'add',
-            path: 'src/components/Elements/index.ts',
+            path: 'src/components/{{pascalCase component}}/{{pascalCase component}}.tsx',
+            templateFile: 'generator/component/Component.tsx.hbs'
+          },
+          {
+            type: 'add',
+            path: 'src/components/{{pascalCase component}}/index.ts',
+            templateFile: 'generator/component/index.ts.hbs'
+          }
+        )
+
+        return actions
+      }
+
+      if (choice === 'components/Elements/*') {
+        actions.push(
+          {
+            type: 'add',
+            path: 'src/componentss/Elements/{{pascalCase component}}/index.ts',
+            templateFile: 'generator/component/index.ts.hbs'
+          },
+          {
+            type: 'add',
+            path: 'src/componentss/Elements/{{pascalCase component}}/{{pascalCase component}}.tsx',
+            templateFile: 'generator/component/Component.tsx.hbs'
+          },
+          plop.getParentIndexAction(component, {
+            target: 'src/componentss/Elements/index.ts',
             templateFile: 'generator/component/index.ts.hbs'
           })
+        )
 
-      return actions
+        return actions
+      }
     }
-  }
+  })
 }
--- frontend/generator/feature/index.js
+++ frontend/generator/feature/index.js
@@ -1,59 +1,61 @@
-module.exports = {
-  description: 'src/features/*',
-  prompts: [
-    {
-      type: 'input',
-      name: 'feature',
-      message: 'kebabCase feature name'
-    },
-    {
-      type: 'checkbox',
-      name: 'choices',
-      message: `\nfeature/components/Feature.tsx\nfeature/index.ts\n以外に事前に作成しておきたいフォルダを選択してください\n`,
-      choices: [
-        'api',
-        'hooks',
-        'types',
+module.exports = function (
+  name = 'feature',
+  /** @type {import('plop').NodePlopAPI} */
+  plop
+) {
+  plop.setGenerator(name, {
+    description: 'src/features/*',
+    prompts: [
+      {
+        type: 'input',
+        name: 'feature',
+        message: 'kebabCase feature name'
+      },
+      {
+        type: 'checkbox',
+        name: 'choices',
+        message: `\nfeature/components/Feature.tsx\nfeature/index.ts\n以外に事前に作成しておきたいフォルダを選択してください\n`,
+        choices: ['api', 'hooks', 'types']
+      }
+    ],
+    actions: ({ choices }) => {
+      const actions = [
+        {
+          type: 'add',
+          path: 'src/features/{{kebabCase feature}}/components/{{pascalCase feature}}.tsx',
+          templateFile: 'generator/feature/components/Component.tsx.hbs'
+        },
+        {
+          type: 'add',
+          path: 'src/features/{{kebabCase feature}}/index.ts',
+          templateFile: 'generator/feature/index.ts.hbs'
+        }
       ]
-    }
-  ],
-  actions: ({ choices }) => {
-    const actions = [
-      {
-        type: 'add',
-        path: 'src/features/{{kebabCase feature}}/components/{{pascalCase feature}}.tsx',
-        templateFile: 'generator/feature/components/Component.tsx.hbs'
-      },
-      {
-        type: 'add',
-        path: 'src/features/{{kebabCase feature}}/index.ts',
-        templateFile: 'generator/feature/index.ts.hbs'
-      }
-    ]
 
-    if (!choices) return actions
-    const choiceAction = {
-      api: {
-        path: 'src/features/{{kebabCase feature}}/api/get{{pascalCase feature}}.ts',
-        templateFile: 'generator/feature/api/getFeature.ts.hbs'
-      },
-      hooks: {
-        path: 'src/features/{{kebabCase feature}}/hooks/use{{pascalCase feature}}.ts',
-        templateFile: 'generator/feature/hooks/useFeature.ts.hbs'
-      },
-      types: {
-        path: 'src/features/{{kebabCase feature}}/types/index.ts',
-        templateFile: 'generator/feature/types/index.ts.hbs'
+      if (!choices) return actions
+      const choiceAction = {
+        api: {
+          path: 'src/features/{{kebabCase feature}}/api/get{{pascalCase feature}}.ts',
+          templateFile: 'generator/feature/api/getFeature.ts.hbs'
+        },
+        hooks: {
+          path: 'src/features/{{kebabCase feature}}/hooks/use{{pascalCase feature}}.ts',
+          templateFile: 'generator/feature/hooks/useFeature.ts.hbs'
+        },
+        types: {
+          path: 'src/features/{{kebabCase feature}}/types/index.ts',
+          templateFile: 'generator/feature/types/index.ts.hbs'
+        }
       }
-    }
 
-    for (const choice of choices) {
-      const options = choiceAction[choice]
-      actions.push({
-        type: 'add',
-        ...options
-      })
+      for (const choice of choices) {
+        const options = choiceAction[choice]
+        actions.push({
+          type: 'add',
+          ...options
+        })
+      }
+      return actions
     }
-    return actions
-  }
+  })
 }
