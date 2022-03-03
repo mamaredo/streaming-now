c0984c5c8ffc9b26f03e4703b11ee5194376f483
D_nishimura <11kakeru30@gmail.com>
2022-03-03 21:25:21 +0900

    add: react-queryのdevtoolsを追加

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { queryClient } from '@/lib/react-query'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
