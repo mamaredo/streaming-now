a7bed27987c50eed86e0f946e77c8ec16f19ba37
D_nishimura <11kakeru30@gmail.com>
2022-03-04 15:26:51 +0900

    update: 返却値をtrue or falseに固定

--- frontend/src/features/twitch-auth/api/twitchAuth.ts
+++ frontend/src/features/twitch-auth/api/twitchAuth.ts
@@ -3,18 +3,20 @@ import { ParsedUrlQuery } from 'querystring'
 import { axios } from '@/lib/axios'
 
 export const twitchAuth = async (params: ParsedUrlQuery) => {
-  let isAuth = false
-  console.log(params)
   const payload = {
     code: params.code,
     scope: params.scope
   }
 
-  // const payload = JSON.stringify(code)
-  await axios.post('/api/twitch-auth', JSON.stringify(payload), {
-    headers: {
-      'Content-Type': 'application/json'
+  const { is_auth }: { is_auth: boolean } = await axios.post(
+    '/api/twitch-auth',
+    JSON.stringify(payload),
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
-  })
-  return isAuth
+  )
+
+  return is_auth
 }
