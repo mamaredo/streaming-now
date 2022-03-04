0688439ac725d5d10317bd8718151c0a3489ec81
D_nishimura <11kakeru30@gmail.com>
2022-03-04 18:36:58 +0900

    update: client_id, callback_urlをconfigからimport

--- frontend/src/features/twitch-auth/components/SignInTwitch.tsx
+++ frontend/src/features/twitch-auth/components/SignInTwitch.tsx
@@ -1,8 +1,10 @@
 import { Link } from '@chakra-ui/react'
 
+import { TWITCH } from '@/config'
+
 export type SignInTwitchProps = {}
 
-const link = `https://id.twitch.tv/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_TWITCH_CALLBACK_URL}&response_type=code&scope=openid`
+const link = `https://id.twitch.tv/oauth2/authorize?client_id=${TWITCH.CLIENT_ID}&redirect_uri=${TWITCH.CALLBACK_URL}&response_type=code&scope=openid user:read:follows`
 
 export const SignInTwitch = () => {
   return <Link href={link}>SignInTwitch</Link>
