96a97812c4fc160c1861b30e4122e966d97155a1
D_nishimura <11kakeru30@gmail.com>
2022-03-04 15:24:10 +0900

    update: response返却時にcookieを設定

--- backend/api/twitch/auth.go
+++ backend/api/twitch/auth.go
@@ -8,8 +8,8 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
-	_ "github.com/gorilla/sessions"
 	"github.com/mamaredo/streaming-now/pkg/env"
 )
 
@@ -22,7 +22,6 @@ import (
 // 	cookieStore  = sessions.NewCookieStore(cookieSecret)
 // )
 
-// cookieにアクセストークン, id_tokenを乗せてレスポンスを返す
 type Response struct {
 	Is_auth bool `json:"is_auth"`
 }
@@ -34,34 +33,34 @@ func Auth(w http.ResponseWriter, r *http.Request) {
 	}
 
 	q := r.URL.Query()
-	// c, _ := r.Cookie("session")
 	token, err := requestToken(q, r)
 	if err != nil {
 		log.Println(err)
+		json.NewEncoder(w).Encode(Response{
+			Is_auth: true,
+		})
+		return
 	}
 
-	accessTokenCookie := &http.Cookie{
-		Name:  "twitch_access_token",
-		Value: token["access_token"].(string),
-	}
-	idTokenCookie := &http.Cookie{
-		Name:  "twitch_id_token",
-		Value: token["id_token"].(string),
-	}
-	http.SetCookie(w, accessTokenCookie)
-	http.SetCookie(w, idTokenCookie)
 	id_token := token["id_token"].(string)
 	if err := VerifySignature(&id_token); err != nil {
+		log.Println("token signature Error")
+		json.NewEncoder(w).Encode(Response{
+			Is_auth: true,
+		})
 		return
 	}
 
-	var isAfterExp bool
-	isAfterExp, err = VerifyIdToken(&id_token)
+	verifyIdToken, err := VerifyIdToken(&id_token)
 	if err != nil {
+		log.Println("token verify Error")
+		json.NewEncoder(w).Encode(Response{
+			Is_auth: true,
+		})
 		return
 	}
 
-	if isAfterExp {
+	if time.Now().After(verifyIdToken.Expiration()) {
 		log.Println("[Twitch] IDToken invalid exp :(")
 		_, err := requestToken(q, r)
 		if err != nil {
@@ -69,10 +68,24 @@ func Auth(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	response := Response{
-		Is_auth: true,
+	accessTokenCookie := &http.Cookie{
+		Name:   "twitch_access_token",
+		Value:  token["access_token"].(string),
+		MaxAge: int(token["expires_in"].(float64)),
+		Path:   "/",
 	}
-	json.NewEncoder(w).Encode(response)
+	idTokenCookie := &http.Cookie{
+		Name:    "twitch_id_token",
+		Value:   token["id_token"].(string),
+		Expires: verifyIdToken.Expiration(),
+		Path:    "/",
+	}
+	http.SetCookie(w, accessTokenCookie)
+	http.SetCookie(w, idTokenCookie)
+
+	json.NewEncoder(w).Encode(Response{
+		Is_auth: true,
+	})
 }
 
 type AuthorizationBody struct {
