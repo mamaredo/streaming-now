cf47caab78732f9c5959d9c2647ed265aae5d1d5
D_nishimura <11kakeru30@gmail.com>
2022-03-06 01:56:30 +0900

    create: getActiveStreamerを作成

--- frontend/src/features/streamer/api/getStreamer.ts
+++
@@ -1,9 +0,0 @@
-import { axios } from '@/lib/axios'
-
-import { Endpoint } from '../types'
-
-const endpoint: Endpoint['streamer'] = '/api/twitch/active-streamer'
-
-export const getStreamer = async () => {
-  await axios.get(endpoint)
-}
