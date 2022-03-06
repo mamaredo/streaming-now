39e2de2f80d980d2a27d6e872e53dbcac680553a
D_nishimura <11kakeru30@gmail.com>
2022-03-06 17:19:25 +0900

    update: MainLayout, Headingを使うように更新

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,20 @@
 import type { NextPage } from 'next'
 
+import { Heading } from '@/components/Elements'
 import { Head } from '@/components/Head'
+import { MainLayout } from '@/components/Layout'
 import { SignInTwitch } from '@/features/twitch-auth'
 
 const Home: NextPage = () => {
   return (
     <>
       <Head />
-      <div>
-        <main>
-          <div>Hello world</div>
-          <SignInTwitch />
-        </main>
-        <footer></footer>
-      </div>
+      <MainLayout>
+        <Heading as="h1" fontSize="2xl" color="text-primary">
+          Streaming Now?
+        </Heading>
+        <SignInTwitch />
+      </MainLayout>
     </>
   )
 }
