754519df15598d01354a34d92de2985653dd9291
D_nishimura <11kakeru30@gmail.com>
2022-03-06 11:47:08 +0900

    update: 横幅を超えた文字が三点リーダーになるように

--- frontend/src/features/streamer/components/ActiveStreamer.tsx
+++ frontend/src/features/streamer/components/ActiveStreamer.tsx
@@ -28,10 +28,11 @@ export const ActiveStreamer = () => {
             <StreamerIcon src={data.profile_img} name={data.user_name} />
           </Link>
           <Heading
-            sx={{ paddingTop: '4px' }}
+            sx={{ paddingTop: '4px', width: { base: '72px', xl: '96px' } }}
             as="h3"
             size="base"
             textAlign="center"
+            isTruncated
           >
             {data.user_name}
           </Heading>
