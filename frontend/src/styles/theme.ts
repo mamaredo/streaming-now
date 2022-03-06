918be94dffa4857e7a3e1017e04d4a36fda33424
D_nishimura <11kakeru30@gmail.com>
2022-03-06 13:46:46 +0900

    add: colors, styles.globalを追加

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -2,10 +2,28 @@ import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
   semanticTokens: {
-    colors: {},
+    colors: {
+      primary: {
+        default: '#120052'
+      },
+      secondary: {
+        default: '#FF2C6C'
+      },
+      text: {
+        default: '#2B2B2B'
+      }
+    },
     fonts: {
       heading: 'Righteous, cursive;',
       body: 'Righteous, cursive;'
+    }
+  },
+  styles: {
+    global: {
+      body: {
+        letterSpacing: '0.5px',
+        background: '#FAFAFA'
+      }
     }
   }
 })
