73a628f2315cf1320adc48f085eb1ea42cf404b9
D_nishimura <11kakeru30@gmail.com>
2022-03-06 14:03:00 +0900

    create: h tagで使用するHeading.tsxを作成

---
+++ frontend/src/components/Elements/Heading/Heading.tsx
@@ -0,0 +1,28 @@
+import { Heading as ChakraHeading, TypographyProps } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+
+export type HeadingProps = {
+  children: ReactNode
+  as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+  fontSize: TypographyProps['fontSize']
+  color?: string
+}
+
+export const Heading = ({
+  children,
+  as,
+  fontSize,
+  color = 'text'
+}: HeadingProps) => {
+  return (
+    <ChakraHeading
+      as={as}
+      color={color}
+      fontWeight="400"
+      letterSpacing="0.25px"
+      fontSize={fontSize}
+    >
+      {children}
+    </ChakraHeading>
+  )
+}
