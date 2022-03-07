202cc524837c3b4417cec448178578b04786687e
D_nishimura <11kakeru30@gmail.com>
2022-03-08 06:04:33 +0900

    add: urlの追加

--- frontend/src/features/youtube-auth/components/YoutubeAuth.tsx
+++ frontend/src/features/youtube-auth/components/YoutubeAuth.tsx
@@ -1,8 +1,9 @@
 import { StreamingServiceLink } from '@/components/Elements'
+import { YOUTUBE } from '@/config'
 
 export type YoutubeAuthProps = {}
 
-const url = ''
+const url = `https://accounts.google.com/o/oauth2/auth?client_id=${YOUTUBE.CLIENT_ID}&redirect_uri=${YOUTUBE.CALLBACK_URL}&scope=https://www.googleapis.com/auth/youtube.readonly&response_type=code&access_type=offline`
 
 export const YoutubeAuth = () => {
   return (
