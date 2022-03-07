e8083c0016fd8f1cd96ae7fa58424136db25cc8b
D_nishimura <11kakeru30@gmail.com>
2022-03-08 06:04:21 +0900

    add: requestを追加

--- frontend/src/features/youtube-auth/api/youtubeAuth.ts
+++ frontend/src/features/youtube-auth/api/youtubeAuth.ts
@@ -1,1 +1,26 @@
-export const youtubeAuth = () => {}
+import { ParsedUrlQuery } from 'querystring'
+
+import { axios } from '@/lib/axios'
+
+import { Endpoint } from '../types'
+
+const endpoint: Endpoint['auth'] = '/api/youtube/auth'
+
+export const youtubeAuth = async (params: ParsedUrlQuery) => {
+  const payload = {
+    code: params.code,
+    scope: params.scope
+  }
+
+  const { is_auth }: { is_auth: boolean } = await axios.post(
+    endpoint,
+    JSON.stringify(payload),
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  )
+
+  return is_auth
+}
