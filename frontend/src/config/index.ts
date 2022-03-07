312044ccdf05b663d1de9524038bf19dd1e66359
D_nishimura <11kakeru30@gmail.com>
2022-03-08 06:02:39 +0900

    add: youtubeの設定を追加

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -2,3 +2,8 @@ export const TWITCH = {
   CLIENT_ID: process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID as string,
   CALLBACK_URL: process.env.NEXT_PUBLIC_TWITCH_CALLBACK_URL as string
 }
+
+export const YOUTUBE = {
+  CLIENT_ID: process.env.NEXT_PUBLIC_YOUTUBE_CLIENT_ID as string,
+  CALLBACK_URL: process.env.NEXT_PUBLIC_YOUTUBE_CALLBACK_URL as string
+}
