b75b86364fba4d27136c9bf9d1d37d6add580533
D_nishimura <11kakeru30@gmail.com>
2022-03-07 14:46:12 +0900

    update: geSleepStreamerの完成

--- backend/api/twitch/getSleepStreamer.go
+++ backend/api/twitch/getSleepStreamer.go
@@ -1,34 +1,31 @@
 package twitch
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"sort"
+	"strconv"
 )
 
-// type sleepStreamerResponseModel struct {
-// 	User_name   string `json:"user_name"`
-// 	Profile_img string `json:"profile_img"`
-// 	Stream_link string `json:"stream_link"`
-// }
+type sleepStreamerResponseModel struct {
+	User_name   string `json:"user_name"`
+	Profile_img string `json:"profile_img"`
+	Stream_link string `json:"stream_link"`
+}
 
-// type sleepStreamerResponse struct {
-// 	Data []sleepStreamerResponseModel `json:"data"`
-// }
+type sleepStreamerResponse struct {
+	Data []sleepStreamerResponseModel `json:"data"`
+}
 
 func GetSleepStremaer(w http.ResponseWriter, r *http.Request) {
 	token := VerifyAccessToken(w, r)
 
-	// f, err := GetFollowActiveStreamer(token)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	w.Write([]byte("400 Bad Request"))
-	// 	return
-	// }
-
-	// uids := []string{}
-	// for _, v := range f.Data {
-	// 	uids = append(uids, v.User_id)
-	// }
+	au, err := GetFollowActiveStreamer(token)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("400 Bad Request"))
+		return
+	}
 
 	fu, err := GetFollowsUsers(token)
 	if err != nil {
@@ -37,8 +34,74 @@ func GetSleepStremaer(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// 正確なresponseを取得したところまで確認
-	fmt.Printf("%+v", *fu)
-	fmt.Printf("%+v", len(fu.Data))
+	aUids := []int{}
+	for _, v := range au.Data {
+		intValue, _ := strconv.Atoi(v.User_id)
+		aUids = append(aUids, intValue)
+	}
 
+	fUids := []int{}
+	for _, v := range fu.Data {
+		intValue, _ := strconv.Atoi(v.To_id)
+		fUids = append(fUids, intValue)
+	}
+
+	sleepUids := []string{}
+	if len(aUids) != len(fUids) {
+		sUids := filterUser(aUids, fUids)
+		for i := 0; i < len(sUids); i++ {
+			strValue := strconv.Itoa(sUids[i])
+			sleepUids = append(sleepUids, strValue)
+		}
+	} else {
+		for i := 0; i < len(aUids); i++ {
+			strValue := strconv.Itoa(aUids[i])
+			sleepUids = append(sleepUids, strValue)
+		}
+	}
+
+	u, err := GetUser(token, sleepUids)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("400 Bad Request"))
+		return
+	}
+
+	res := sleepStreamerResponse{
+		Data: nil,
+	}
+
+	for i := 0; i < len(u.Data); i++ {
+		res.Data = append(res.Data, sleepStreamerResponseModel{
+			User_name:   u.Data[i].Display_name,
+			Profile_img: u.Data[i].Profile_image_url,
+			Stream_link: "https://www.twitch.tv/" + u.Data[i].Display_name,
+		})
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
+
+func filterUser(activeAll []int, followAll []int) []int {
+	results := []int{}
+	sort.Sort(sort.Reverse(sort.IntSlice(activeAll))) // 降順
+	sort.Ints(followAll)                              // 昇順
+
+	var count = 0
+	for 0 < len(activeAll) {
+
+		for j := 0; j < len(followAll); j++ {
+			if activeAll[0] < followAll[j] {
+				results = append(results, followAll[j+1:]...)
+				followAll = followAll[:j]
+				break
+			}
+			results = append(results, followAll[j])
+			count += 1
+		}
+		activeAll = activeAll[1:]
+	}
+	results = append(results, followAll[:]...)
+
+	return results
 }
